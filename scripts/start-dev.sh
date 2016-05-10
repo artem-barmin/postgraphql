5433e75eb99ccda28fa5203145834889b6637a25
artem.barmin <artem.barmin@gmail.com>
2016-05-10 18:03:06 +0300

    client.js

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 babel-watch --watch src \
-  src/main.js -- postgres://postgres:postgres@localhost:5432/political --development -p 5000 -n 0.0.0.0
+  src/main.js -- postgres://postgres:postgres@localhost:5432/postgraph_test --development -p 5000 -n 0.0.0.0
